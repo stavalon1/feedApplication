c80e1f12cb01d2e61ff740796760ce6333ccc927
stav <stavalon31@gmail.com>
2021-07-12 23:54:30 +0300

    version_3

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput,
   TouchableOpacity, ActivityIndicator, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import AddProducts from './screens/addProduct';
-import feedScreen from './screens/feed'; 
-import {navigationRef} from './navigator/rootNavigation';
+import AA from './screens/addProduct'
+import BB from './screens/feed' 
+import {navigationRef} from './navigation/root';
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
+   return (
+     
     <NavigationContainer>
     <Stack.Navigator>
-    <Stack.Screen name="Products" component={feedScreen} />
-    <Stack.Screen name="AddProduct" component={AddProducts} />
+    <Stack.Screen name="Products" component={BB} />
+    <Stack.Screen name="AddProduct" component={AA} />
     </Stack.Navigator>
     </NavigationContainer>
  )
+  
 }
 
 const styles = StyleSheet.create({
+  feedBtn: { width: '50%', backgroundColor: '#761954', alignItems: 'center', justifyContent: 'center' },
+  addButton: { width: '50%', backgroundColor: '#e1c13b', alignItems: 'center', justifyContent: 'center' },
+  mainView: { height: '90%', width:'100%', alignItems: 'center', justifyContent: 'center', flex:1 },
+  btnText: { color: '#ffffff', fontSize: 20 },
+  menuView: { height: '10%', flexDirection: 'row', justifyContent: 'space-between' },
   container: {
     flex: 1,
     backgroundColor: '#fff',
--- screens/feed.js
+++ screens/feed.js
@@ -1,70 +1,106 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput,
+import { StyleSheet, Text, View,ScrollView,
   TouchableOpacity, ActivityIndicator, FlatList,Button,Image,Platform} from 'react-native';
-import styles from '../components/styleFeed';
-import {Input} from 'react-native-elements';
-export default function AddProduct({navigation}) {
-
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-
-    const addNewProduct = () => {
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      myHeaders.append("Accept", "application/json");
-      
-      var raw = JSON.stringify({
-        "publisher": {
-          "name": "stav",
-          "id": 315923847,
-          "email": "stavalon31@gmail.com",
-          "mobile": "0535284289"
-        },
-        "productName": name,
-        "productPrice": price,
-        "productImage": imageUrl,
-      });
-      
-      var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-      
-      fetch("http://retailsapi.us-east-2.elasticbeanstalk.com/api/feedapp/addfeed", requestOptions)
-        .then(response => response.text())
-        .then(result =>{navigation.goBack()})
-        .catch(error => console.log('error', error));
-    };
-    return (
-      <View style={styles.mainView}>
-      <Input
-    placeholder="Enter product name"
-    label="Name"
-    leftIcon={{type: 'material', name:'badge'}}
-    value={name}
-    onChangeText={text => setName(text)}
-    />
-    <Input
-    placeholder="Enter your price"
-    label="Price"
-    leftIcon={{type: 'material', name:'payment'}}
-    value={price}
-    onChangeText={text => setPrice(text)}
-    />
-     <Input
-    placeholder="Enter your image URL"
-    label="Product Picture"
-    leftIcon={{type: 'material', name:'face'}}
-    value={imageUrl}
-    onChangeText={text => setImageUrl(text)}
-    />
+import pageStyle from '../components/styleFeed';
+export default function ProductList({navigation}) {
   
-    <TouchableOpacity onPress={addNewProduct} style={styles.AddBtn}><Text style={styles.btnText}>Add new product</Text></TouchableOpacity>
+   const [producuts,setProducts]=useState([])
+   const [val,setVal]=useState(1);
+
+
+   useEffect(() => {
+     if(val==1)
+       loadData();
     
+  }, );
+  function clean(object) {
+    Object
+        .entries(object)
+        .forEach(([k, v]) => {
+            if (v && typeof v === 'object') {
+                clean(v);
+            }
+            if (v && typeof v === 'object' && !Object.keys(v).length || v === null || v === undefined ||v=="") {
+                if (Array.isArray(object)) {
+                    object.splice(k, 1);
+                } else {
+                    object[k]="untitled";
+                }
+            }
+        });
+    return object;
+}
+
+  const loadData = async () => {
+    const response = await fetch('http://retailsapi.us-east-2.elasticbeanstalk.com/api/feedapp/getfeed',
+      {
+        method: 'GET'
+      });
+
+    const data = await response.json();
+    setProducts(data.products);
+    setVal(0)
+   
+ 
+  }
+        
+    return (
+    <View style={pageStyle.pageContainer}>
+       <View style={pageStyle.ProductListContainer}>
+       <ScrollView vertical>
+          {producuts.map((product,key)=>{
+            
+              if(product.publisher!==undefined){
+                
+                return(
+                  <View style={pageStyle.productContainer} key={product.id}>
+                    <View style={pageStyle.imageContainer}>
+                      <Image source={{uri:product.productImage}}
+                        style={{width:"100%", height: "100%",justifyContent:'center'}}></Image>
+                    </View>
+                    <View style={pageStyle.contentContainer}>
+                      <View style={pageStyle.authorRowContainer}><Text style={pageStyle.baseText}><Text style={{fontSize:20}}>publisher</Text></Text></View>
+                      <View style={pageStyle.rowContainer}>
+                          <View  style={pageStyle.authorTextBlockContainer}><Text style={pageStyle.baseText}>Name: </Text></View>
+                          <View style={pageStyle.authorTextBoxContainer}><Text style={pageStyle.baseText}>{product.publisher.name}</Text></View>
+                      </View>
+                      <View style={pageStyle.rowContainer}>
+                          <View  style={pageStyle.authorTextBlockContainer}><Text style={pageStyle.baseText}>Id: </Text></View>
+                          <View style={pageStyle.authorTextBoxContainer}><Text style={pageStyle.baseText}>{product.publisher.id}</Text></View>
+                      </View>
+                      <View style={pageStyle.rowContainer}>
+                          <View  style={pageStyle.authorTextBlockContainer}><Text style={pageStyle.baseText}>Email: </Text></View>
+                          <View style={pageStyle.authorTextBoxContainer}><Text style={pageStyle.baseText}>{product.publisher.email}</Text></View>
+                      
+                      </View>
+                      <View style={pageStyle.rowContainer}>
+                          <View  style={pageStyle.authorTextBlockContainer}><Text style={pageStyle.baseText}>Phone: </Text></View>
+                          <View style={pageStyle.authorTextBoxContainer}><Text style={pageStyle.baseText}>{product.publisher.mobile}</Text></View>
+                      
+                      </View>
+                    </View>
+                    <View style={pageStyle.contentContainer}>
+                      <View style={pageStyle.rowContainer}>
+                          <View  style={pageStyle.productTextBlockContainer}>
+                            <Text style={pageStyle.baseText}>Product Name:</Text>
+                            <Text style={pageStyle.baseText}>{product.productName}</Text>
+                            <Text></Text>
+                            <Text style={pageStyle.baseText}>Product Price:</Text>
+                            <Text style={pageStyle.baseText}>{product.productPrice}</Text>
+                            </View>
+                      </View>                           
+                    </View>
+                 </View>
+                )
+              }  
+          })}  
+        </ScrollView>
+           
+       </View>
+       <View style={pageStyle.menuView}>
+       <TouchableOpacity style={pageStyle.feedBtn} onPress={()=>{navigation.navigate("AddProduct")}}>
+              <Text style={pageStyle.btnText}>Add Product</Text></TouchableOpacity>
+        </View>
     </View>
     );
 }
